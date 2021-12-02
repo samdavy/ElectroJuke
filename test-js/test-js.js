ee00ba3bdc64a0a50915888dbb9b316892bd9fea
Samuel Davy <a01839697@usu.edu>
2021-12-01 23:23:31 -0700

    joe momma

--- test-js/test-js.js
+++ test-js/test-js.js
@@ -2,3 +2,134 @@ const songs = ['song0', 'song1', 'song2'];
 const listItems = songs.map((song) => <li>{song}</li>);
 
 console.log(listItems);
+
+let songs = {
+    songList: [
+      {
+        name: "Dummy Song 1",
+        artist: "Dummy Artist 1",
+        imageUrl: "DummyImage",
+        id: "1"
+      },
+      {
+        name: "Dummy Song 1",
+        artist: "Dummy Artist 1",
+        imageUrl: "DummyImage",
+        id: "2"
+      },
+      {
+        name: "Dummy Song 1",
+        artist: "Dummy Artist 1",
+        imageUrl: "DummyImage",
+        id: "3"
+      },
+      {
+        name: "Dummy Song 1",
+        artist: "Dummy Artist 1",
+        imageUrl: "DummyImage",
+        id: "4"
+      },
+      {
+        name: "Dummy Song 1",
+        artist: "Dummy Artist 1",
+        imageUrl: "DummyImage",
+        id: "5"
+      },
+      {
+        name: "Dummy Song 1",
+        artist: "Dummy Artist 1",
+        imageUrl: "DummyImage",
+        id: "6"
+      },
+      {
+        name: "Dummy Song 1",
+        artist: "Dummy Artist 1",
+        imageUrl: "DummyImage",
+        id: "7"
+      },
+      {
+        name: "Dummy Song 1",
+        artist: "Dummy Artist 1",
+        imageUrl: "DummyImage",
+        id: "8"
+      },
+      {
+        name: "Dummy Song 1",
+        artist: "Dummy Artist 1",
+        imageUrl: "DummyImage",
+        id: "9"
+      },
+      {
+        name: "Dummy Song 1",
+        artist: "Dummy Artist 1",
+        imageUrl: "DummyImage",
+        id: "10"
+      }
+    ]
+  }
+  
+  const searchList = {
+    songList: [
+      {
+        name: "Dummy Song 1",
+        artist: "Dummy Artist 1",
+        imageUrl: "DummyImage",
+        id: "1"
+      },
+      {
+        name: "Dummy Song 1",
+        artist: "Dummy Artist 1",
+        imageUrl: "DummyImage",
+        id: "2"
+      },
+      {
+        name: "Dummy Song 1",
+        artist: "Dummy Artist 1",
+        imageUrl: "DummyImage",
+        id: "3"
+      },
+      {
+        name: "Dummy Song 1",
+        artist: "Dummy Artist 1",
+        imageUrl: "DummyImage",
+        id: "4"
+      },
+      {
+        name: "Dummy Song 1",
+        artist: "Dummy Artist 1",
+        imageUrl: "DummyImage",
+        id: "5"
+      },
+      {
+        name: "Dummy Song 1",
+        artist: "Dummy Artist 1",
+        imageUrl: "DummyImage",
+        id: "6"
+      },
+      {
+        name: "Dummy Song 1",
+        artist: "Dummy Artist 1",
+        imageUrl: "DummyImage",
+        id: "7"
+      },
+      {
+        name: "Dummy Song 1",
+        artist: "Dummy Artist 1",
+        imageUrl: "DummyImage",
+        id: "8"
+      },
+      {
+        name: "Dummy Song 1",
+        artist: "Dummy Artist 1",
+        imageUrl: "DummyImage",
+        id: "9"
+      },
+      {
+        name: "Dummy Song 1",
+        artist: "Dummy Artist 1",
+        imageUrl: "DummyImage",
+        id: "10"
+      }
+    ]
+  }
+  
